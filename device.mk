a8df71f1df727fbf72f2b52146994a4f1d430253
tilt-rgb <lesma_chan@pm.me>
2025-04-24 18:48:33 +0000

    m52xq: Enable blur

--- device.mk
+++ device.mk
@@ -33,6 +33,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Fingerprint Gestures
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
